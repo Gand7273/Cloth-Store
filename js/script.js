cdebad6f71bece78bd52924c1005b19b597360cb
Gand7273 <davidtassyrov@gmail.com>
2025-01-11 17:48:29 +0500

    fvv

---
+++ js/script.js
@@ -0,0 +1,316 @@
+
+const cards = {
+    1:{
+        img:"img/Rectangle 3 (2).png",
+        cat:["all", "XS", "T-Shirts", ]
+    },
+    2:{
+        img:"img/2x/1x/Ресурс 13.png",
+        cat:["all", "XS", "T-Shirts"]
+    },
+    3:{
+        img:"img/2x/1x/1x/Ресурс 14.png",
+        cat:["all", "S", "T-Shirts"]
+    },
+    4:{
+        img:"img/2x/1x/1x/Ресурс 15.png",
+        cat:["all", "XS", "T-Shirts"]
+    },
+    5:{
+        img:"img/2x/1x/1x/Ресурс 16.png",
+        cat:["all", "XS", "T-Shirts"]
+    },
+    6:{
+        img:"img/2x/1x/1x/Ресурс 17.png",
+        cat:["all", "S", "T-Shirts"]
+    },
+    7:{
+        img:"img/Rectangle 3 (3).png",
+        cat:["all", "XS", "T-Shirts"]
+    },
+    8:{
+        img:"img/delivery-woman-bowing-head-shoulder-t-shirt-cap.jpg",
+        cat:["all", "S", "T-Shirts"]
+    },
+    9:{
+        img:"img/2x/1x/Ресурс 12.png",
+        cat:["all", "XS", "Polo Shirts"]
+    },
+    10:{
+        img:"img/2x/1x/Ресурс 11.png",
+        cat:["all", "2X", "Polo Shirts"]
+    },
+    11:{
+        img:"img/2x/1x/Ресурс 7.png",
+        cat:["all", "2X", "Jeans"]
+    },
+    12:{
+        img:"img/2x/1x/Ресурс 8.png",
+        cat:["all", "XS", "Jeans"]
+    },
+    13:{
+        img:"img/2x/1x/Ресурс 9.png",
+        cat:["all", "2X", "Jeans"]
+    },
+    14:{
+        img:"img/1x/Ресурс 10.png",
+        cat:["all", "XS", "Jeans"]
+    },
+    15:{
+        img:"img/2x/1x/1x/Ресурс 18.png",
+        cat:["all", "S", "Polo Shirts"]
+    },
+    16:{
+        img:"img/2x/1x/1x/Ресурс 19.png",
+        cat:["all", "2X", "Polo Shirts"]
+    },
+    17:{
+        img:"img/2x/1x/1x/Ресурс 20.png",
+        cat:["all", "2X", "Polo Shirts"]
+    },
+    18:{
+        img:"img/2x/1x/1x/Ресурс 21.png",
+        cat:["all", "S", "Polo Shirts"] 
+    },
+    19:{
+        img:"img/2x/1x/1x/Ресурс 22.png",
+        cat:["all", "XS", "Jeans"]
+    },
+    20:{
+        img:"img/2x/1x/1x/Ресурс 23.png",
+        cat:["all", "M", "Jeans"]
+    },
+    21:{
+        img:"img/2x/1x/1x/Ресурс 24.png",
+        cat:["all", "2X", "Jeans"]
+    },
+    22:{
+        img:"img/2x/1x/1x/Ресурс 25.png",
+        cat:["all", "M", "Jeans"]
+    },
+    23:{
+        img:"newimg/1x/Ресурс 26.png",
+        cat:["all", "M", "Shorts"]
+    },
+    24:{
+        img:"newimg/1x/Ресурс 27.png",
+        cat:["all", "2X", "Shorts"]
+    },
+    25:{
+        img:"newimg/1x/Ресурс 28.png",
+        cat:["all", "M", "Shorts"]
+    },
+    26:{
+        img:"newimg/1x/Ресурс 29.png",
+        cat:["all", "XS", "Shorts"]
+    },
+    27:{
+        img:"newimg/1x/Ресурс 33.png",
+        cat:["all", "XL", "Shorts"]
+    },
+    28:{
+        img:"newimg/1x/Ресурс 30.png",
+        cat:["all", "M", "Shorts"]
+    },
+    29:{
+        img:"newimg/1x/Ресурс 31.png",
+        cat:["all", "XL", "Shorts"]
+    },
+    30:{
+        img:"newimg/1x/Ресурс 32.png",
+        cat:["all", "2X", "Shorts"]
+    },
+    31:{
+        img:"newimg/1x/Ресурс 34.png",
+        cat:["all", "M", "Jackets"]
+    },
+    32:{
+        img:"newimg/1x/Ресурс 35.png",
+        cat:["all", "XS", "Jackets"]
+    },
+    33:{
+        img:"newimg/1x/Ресурс 36.png",
+        cat:["all", "L", "Jackets"]
+    },
+    34:{
+        img:"newimg/1x/Ресурс 37.png",
+        cat:["all", "XL", "Jackets"]
+    },
+    35:{
+        img:"newimg/1x/Ресурс 38.png",
+        cat:["all", "L", "Jackets"]
+    },
+    36:{
+        img:"newimg/1x/Ресурс 39.png",
+        cat:["all", "L", "Jackets"]
+    },
+    37:{
+        img:"newimg/1x/Ресурс 40.png",
+        cat:["all", "XS", "Jackets"]
+    },
+    38:{
+        img:"newimg/1x/Ресурс 41.png",
+        cat:["all", "XS", "Jackets"]
+    },
+    39:{
+        img:"newimg/1x/Ресурс 42.png",
+        cat:["all", "L", "Suits"]
+    },
+    40:{
+        img:"newimg/1x/Ресурс 43.png",
+        cat:["all", "XS", "Suits"]
+    },
+    41:{
+        img:"newimg/1x/Ресурс 44.png",
+        cat:["all", "2X", "Suits"]
+    },
+    42:{
+        img:"newimg/1x/Ресурс 45.png",
+        cat:["all", "L", "Suits"]
+    },
+    43:{
+        img:"newimg/1x/Ресурс 46.png",
+        cat:["all", "XL", "Suits"]
+    },
+    44:{
+        img:"newimg/1x/Ресурс 47.png",
+        cat:["all", "XS", "Suits"]
+    },
+    45:{
+        img:"newimg/1x/Ресурс 48.png",
+        cat:["all", "XL", "Suits"]
+    },
+    46:{
+        img:"newimg/1x/Ресурс 49.png",
+        cat:["all", "L", "Suits"]
+    },
+    47:{
+        img:"newimg/1x/Ресурс 13.png",
+        cat:["all", "XL", "Coats"]
+    },
+    48:{
+        img:"newimg/1x/Ресурс 14.png",
+        cat:["all", "L", "Coats"]
+    },
+    49:{
+        img:"newimg/1x/Ресурс 15.png",
+        cat:["all", "XS", "Coats"]
+    },
+    50:{
+        img:'newimg/1x/Ресурс 16.png',
+        cat:["all", "XL", "Coats"]
+    },
+    51:{
+        img:"newimg/1x/Ресурс 17.png",
+        cat:["all", "XL", "Coats"]
+    },
+    52:{
+        img:"newimg/1x/Ресурс 18.png",
+        cat:["all", "XS", "Coats"]
+    },
+    53:{
+        img:"newimg/1x/Ресурс 19.png",
+        cat:["all", "XL", "Coats"]
+    },
+    54:{
+        img:"newimg/1x/Ресурс 20.png",
+        cat:["all", "XS", "Coats"]
+    },
+    55:{
+        img:"newimg/daw/3x/Ресурс 21@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+    56:{
+        img:"newimg/daw/3x/Ресурс 22@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+    57:{
+        img:"newimg/daw/3x/Ресурс 23@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+    58:{
+        img:"newimg/daw/3x/Ресурс 24@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+    59:{
+        img:"newimg/daw/3x/Ресурс 25@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+    60:{
+        img:"newimg/daw/3x/Ресурс 26@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+    61:{
+        img:"newimg/daw/3x/Ресурс 27@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+    62:{
+        img:"newimg/daw/3x/Ресурс 28@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+    63:{
+        img:"newimg/daw/3x/Ресурс 29@3x.png",
+        cat:["all", "XS", "Shirts"]
+    },
+}
+let size = 'all'
+const csds = document.querySelectorAll(".csds")
+const caters = document.querySelectorAll(".form-check-input")
+
+let addww = ["Best Sellers", "T-Shirts", "Jeans", "Jackets", "Coats", "White", "Black", "Grey", "Light blue", "Wheat", "Brown"]
+
+caters.forEach(cater =>{
+    cater.addEventListener("change", ()=>{
+        if(cater.checked){
+            addww.push(cater.dataset.cat)
+            console.log(addww)
+            dwad(size)
+        }
+        else{
+            let newArray= addww.filter(item => item != cater.dataset.cat)
+            addww = newArray
+            console.log(addww)
+            dwad(size)
+        }
+    })
+    
+})
+
+function dwad(c){
+    let card=""
+    const pb = document.querySelector(".pb")
+    for(const content in cards){
+        if(cards[content].cat.includes(c)){
+            if (cards[content].cat.filter(item => addww.includes(item)).length > 0){
+                card+= `<div class="card card_3" style="width: 18rem;">
+    <div class="card__header">
+      <img src="${cards[content].img}" class="card-img-top" alt="...">
+   <div class="button-wrap">
+    
+   </div>
+    </div>
+      <div class="card-body">
+        
+        <h5 class="card-title">V-Neck T-Shirt</h5>
+        <p class="card-text vgr">Embroidered Seersucker Shirt <span>$99</span></p>
+        
+      </div>
+  </div>`
+            }
+            
+        }
+    }
+    pb.innerHTML = card
+}
+dwad(size)
+csds.forEach(btn =>{
+    btn.addEventListener("click", ()=>{
+        size = btn.textContent
+        
+        console.log(addww)
+        dwad(size)
+    })
+})
+
+const l = document.querySelectorAll('.card')
+console.log(l.length)
